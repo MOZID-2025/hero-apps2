78c07a226c630bea991a46c572e733f629494650
Abdul Mozid <mozidkhan2020@gmail.com>
2025-10-19 21:21:00 +0600

    nav work

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { Link, NavLink } from "react-router";
+import { NavLink, Link } from "react-router";
 import logoimg from "../assets/logo.png";
 import github from "../assets/Vector.png";
 
 const Navbar = () => {
+  const links = [
+    { path: "/", label: "Home" },
+    { path: "/apps", label: "Apps" },
+    { path: "/installation", label: "Installation" },
+  ];
+
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="max-w-[1280px] mx-auto navbar bg-base-100">
+      {/* Navbar Start */}
       <div className="navbar-start">
+        {/* Mobile Dropdown */}
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <div tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -16,62 +24,71 @@ const Navbar = () => {
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
-              {" "}
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
                 d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
+              />
             </svg>
           </div>
           <ul
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/apps">Apps</NavLink>
-            </li>
-            <li>
-              <NavLink to="/installation">Installation</NavLink>
-            </li>
+            {links.map((link) => (
+              <li key={link.path}>
+                <NavLink
+                  to={link.path}
+                  className={({ isActive }) =>
+                    `px-3 py-1 rounded-md transition-all duration-300 ${
+                      isActive ? "underline underline-offset-4" : ""
+                    }`
+                  }
+                >
+                  {link.label}
+                </NavLink>
+              </li>
+            ))}
           </ul>
         </div>
-        <div>
-          <Link
-            to="/"
-            className="text-xl flex gap-2 font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#632EE3] to-[#9F62F2]"
-          >
-            <img src={logoimg} alt="" /> HERO.IO
-          </Link>
-        </div>
+
+        {/* Logo */}
+        <Link
+          to="/"
+          className="text-xl flex gap-2 font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#632EE3] to-[#9F62F2]"
+        >
+          <img src={logoimg} alt="" /> HERO.IO
+        </Link>
       </div>
 
-      <div className="navbar-center ">
-        <ul className="menu menu-horizontal px-1 hidden lg:flex text-[18px]">
-          <li>
-            <NavLink className="" to="/">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/apps">Apps</NavLink>
-          </li>
-          <li>
-            <NavLink to="/installation">Installation</NavLink>
-          </li>
+      {/* Navbar Center (Desktop Menu) */}
+      <div className="navbar-center">
+        <ul className="menu menu-horizontal px-1 hidden lg:flex text-[18px] gap-4">
+          {links.map((link) => (
+            <li key={link.path}>
+              <NavLink
+                to={link.path}
+                className={({ isActive }) =>
+                  `px-3 py-1 rounded-md transition-all duration-300 ${
+                    isActive ? "underline underline-offset-4" : ""
+                  } hover:text-[#632EE3]`
+                }
+              >
+                {link.label}
+              </NavLink>
+            </li>
+          ))}
         </ul>
       </div>
+
+      {/* Navbar End */}
       <div className="navbar-end">
         <NavLink
           to="https://github.com/MOZID-2025"
-          className="btn bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white"
+          className="btn bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white flex items-center gap-2"
         >
-          <img src={github} alt="" />
-          Contribute
+          <img src={github} alt="" /> Contribute
         </NavLink>
       </div>
     </div>
